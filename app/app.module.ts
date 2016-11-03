b7d6cf319c196e3bbed5e5c9071fd006e401ee84
Surya Penmetsa <Surya Penmetsa>
2016-11-03 17:57:02 +0530

    initial commit

---
+++ app/app.module.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by Surya on 11/3/2016.
+ */
+import { NgModule      } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent  } from './app.component';
+
+
+@NgModule({
+    imports:        [BrowserModule],
+    declarations:   [AppComponent],
+    bootstrap:      [AppComponent]
+})
+export class AppModule { }
